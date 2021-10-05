48509b99e9dadb02afdd692f6f1230f83e80420d
chamikagangul <pchamikagangul@gmail.com>
2021-10-05 14:47:18 +0530

    update Test foy cyclicity

--- linked lists/Test for overlapping lists-lists withLoops.py
+++ linked lists/Test for overlapping lists-lists withLoops.py
@@ -21,7 +21,6 @@ class Node:
         node.next = node.next.next
 
     def print_list(self):
-        input()
         if(self == None ):
             return
         else:
@@ -34,70 +33,78 @@ class Node:
 
 
 
-def createLinkedListByList(L,i_):
-    i = len(L) - i_
-    # print(i,i_,len(L))
+def createLinkedListByList(L):
     lastNode =None
     isFirst = True
-    count = 0
     for x in L[::-1]:
         n = Node(x,lastNode)
         lastNode = n
         if(isFirst):
             finalNode = lastNode
             isFirst = False
-        if(i==count):
-            finalNode.next = n
-        count+=1
+    # finalNode.next = n
     return n
 
-def isCyclic(LL):
+def joinLists(L1,L2,n):
+
+    r1,r2 = L1,L2
+    while(L1.next):
+        # print(L1.data)
+        L1 = L1.next
     
-    def getCycleLength(node):
-        x = node.next
-        count = 1
-        while(x!=node):
-            x = x.next
-            count+=1
-        return count
+    for i in range(n):
+        L2 = L2.next
+    L1.next = L2
+
+    return r1,r2
+
+def checkForOverlapping(L1,L2):
+    r1 = L1
+    r2 = L2
+    L1Length = 0
+    L2Length = 0
+    while(L1.next):
+        L1Length +=1
+        L1 = L1.next
+    while(L2.next):
+        L2Length +=1
+        L2 = L2.next
+    
+    
+    if(L1Length>L2Length):
+        L1,L2 = r1,r2
+    else:
+        L1,L2 = r2,r1
+
+
+    for i in range(L1Length-L2Length):
+        L1 = L1.next
+    
+    while(L1!=L2):
+        L1=L1.next
+        L2=L2.next
+    else:
+        return L1
 
     
-    slow = LL
-    fast = LL.next
-    go = False
-    while(slow!=fast):
-        # print("isCyclic- ",slow,fast)
-        if(fast == None):
-            break
-
-        fast = fast.next
-        if(go):
-            slow= slow.next
-        go = ~go  
-    
-    cycleLen = getCycleLength(slow)
-    itr1 = LL
-    itr2 = LL
-    for _ in range(cycleLen):
-        itr1 = itr1.next
-    
-    while(itr1!=itr2):
-        itr1,itr2 = itr1.next,itr2.next
-    return itr1
-
 
 
 
 L1 = [randint(0, 1000) for _ in range(10)]
-
-print(L1,)
-L1 = createLinkedListByList(L1,3)
+L2 = [randint(0, 1000) for _ in range(10)]
 
 
-print(isCyclic(L1))
-# L1.print_list()
+L1 = createLinkedListByList(L1)
+L2 = createLinkedListByList(L2)
 
-# L1.print_list()
+L1,L2 = joinLists(L1,L2,4)
+
+L1.print_list()
+L2.print_list()
+
+print(checkForOverlapping(L1,L2))
+
+
 
 
 
