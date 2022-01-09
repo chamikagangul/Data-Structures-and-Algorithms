a4be549ba2504ead7d812da67f18b2b7beccefe3
chamikagangul <59189373+chamikagangul@users.noreply.github.com>
2022-01-09 16:01:56 +0530

    Create decoratorsTest.py

---
+++ test/decoratorsTest.py
@@ -0,0 +1,14 @@
+def testDecorator(func):
+    def inner(*args, **kwargs):
+        print(args,kwargs)
+        print("before")
+        func(*args, **kwargs)
+        print("after")
+    return inner
+
+@testDecorator
+def printName(name):
+    print("My name is",name)
+
+
+printName("chamika")
